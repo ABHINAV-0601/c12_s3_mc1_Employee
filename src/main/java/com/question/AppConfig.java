faef0d987e106e20bcd28d26b84da4a65fea544d
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-10-31 16:16:12 +0530

    completed the tasks

---
+++ src/main/java/com/question/AppConfig.java
@@ -0,0 +1,19 @@
+package com.question;
+
+import org.springframework.context.annotation.Bean;
+
+public class AppConfig {
+    @Bean("employee")
+    public Employee getEmployeeDetails(){
+        Employee employee = new Employee(getDepartmentDetails());
+        employee.setEmpId(101);
+        employee.setEmpName("Thomas Shelby");
+        employee.setSalary(50000.50);
+        return employee;
+    }
+
+    @Bean
+    public Department getDepartmentDetails(){
+        return new Department(1,"Investigation");
+    }
+}
